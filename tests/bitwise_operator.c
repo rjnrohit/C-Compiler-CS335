226a08cb09e136085410089978f0aad1d1794f6a
Akshaj Bansal <45758171+akshajbansal@users.noreply.github.com>
2021-02-07 22:10:28 +0530

    Update bitwise_operator.c

--- tests/bitwise_operator.c
+++ tests/bitwise_operator.c
@@ -1,24 +1,30 @@
 int main(){
     char operator;
-    printf("Enter an bitwise operator (&, |, ^, ~): ");
-    scanf("%c",&operator);
+    printf("Enter a bitwise operator (&, |, ^, ~, <<, >>): ");
+    scanf("%c", &operator);
 
     long a = 0x56a, b = 0x44f;
     bool equal = (a==b);
     printf("%d\n", ((a!=b) == equal));
 
-    if(operator=='|'){
+    if(operator == '|'){
         printf("%d", a|b);
     }
-    else if(operator=='&'){
+    else if(operator == '&'){
         printf("%d", a&b);
     }
-    else if(operator=='^'){
+    else if(operator == '^'){
         printf("%d", a^b);
     }
-    else if(operator=='~'){
+    else if(operator == '~'){
         printf("%d %d", ~a, ~b);
     }
+    else if(operator == '<<'){
+        printf("%d %d", a<<1, b<<2);
+    }
+    else if(operator == '>>'){
+        printf("%d %d", a>>1, b>>2);
+    }
     
-    printf("\n%d\n%d\n%d\n%d\n%d",a+b,a-b,a*b,a/b,a%b);
+    printf("\n%d\n%d\n%d\n%d\n%d", a+b, a-b, a*b, a/b, a%b);
 }
