f30fd429d6438515258ff2caa09851e3bf71e45b
Akshaj Bansal <45758171+akshajbansal@users.noreply.github.com>
2021-02-07 22:05:07 +0530

    Update nth_fib.c

--- tests/nth_fib.c
+++ tests/nth_fib.c
@@ -3,14 +3,14 @@ enum day {sunday, monday, tuesday, wednesday, thursday, friday, saturday};
 int main()
 {
 	int n;
-	scanf("%d",&n);
+	scanf("%d", &n);
 
 	long a, b;
 	long long temp;
 	
-	for(int d = sunday;d<=saturday;d++){
+	for(int d = sunday; d <= saturday; d++){
 		a = 0, b = 1;
-		for(int i = 2;i<=d;++i)
+		for(int i = 2; i <= d; ++i)
 		{
 			temp = b;
 			b = a + b;
