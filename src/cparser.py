94709ccbc9ee4dceead10fa693b99fa4ad04952c
Rohit Ranjan <rohitrjn629@gmail.com>
2021-02-15 23:32:24 +0530

    add cparser modal file

---
+++ src/cparser.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""This Script for Syntax Analysis of C code
+provided by user"""
+
+import sys
+import ply.lex as lex
+import ply.yacc as yacc
+from . import clexer as lexer
+
+#####################Grammar section #################
+
+
+######################################################
+
+def main():
+    """Driver code for Abstract syntax tree 
+    Generation"""
+    pass
+
+if __name__ == "__main__":
+    main()
+
+
+"""
+#refrences: https://www.dabeaz.com/ply/ply.html
+"""
