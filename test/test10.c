2a56c5245aebfa8ff5e46a1ca4225362f0091de0
Varchasv Shri <varchasv@iitk.ac.in>
2021-04-08 20:40:11 +0530

    Update test10.c

--- test/test10.c
+++ test/test10.c
@@ -1,68 +1,25 @@
-void debug_mutex_lock_common(struct mutex *lock, struct mutex_waiter *waiter)
+struct Node {
+	int data;
+	struct Node* next;
+};
+
+int main()
 {
-	memset(waiter, MUTEX_DEBUG_INIT, sizeof(*waiter));
-	waiter->magic = waiter;
-	INIT_LIST_HEAD(&waiter->list);
-}
+	struct Node head;
+	struct Node second;
+	struct Node third;
 
-void debug_mutex_wake_waiter(struct mutex *lock, struct mutex_waiter *waiter)
-{
-	lockdep_assert_held(&lock->wait_lock);
-	DEBUG_LOCKS_WARN_ON(list_empty(&lock->wait_list));
-	DEBUG_LOCKS_WARN_ON(waiter->magic != waiter);
-	DEBUG_LOCKS_WARN_ON(list_empty(&waiter->list));
-}
-
-void debug_mutex_free_waiter(struct mutex_waiter *waiter)
-{
-	DEBUG_LOCKS_WARN_ON(!list_empty(&waiter->list));
-	memset(waiter, MUTEX_DEBUG_FREE, sizeof(*waiter));
-}
-
-void debug_mutex_add_waiter(struct mutex *lock, struct mutex_waiter *waiter,
-			    struct task_struct *task)
-{
-	lockdep_assert_held(&lock->wait_lock);
-
-	/* Mark the current thread as blocked on the lock: */
-	task->blocked_on = waiter;
-}
-
-void mutex_remove_waiter(struct mutex *lock, struct mutex_waiter *waiter,
-			 struct task_struct *task)
-{
-	DEBUG_LOCKS_WARN_ON(list_empty(&waiter->list));
-	DEBUG_LOCKS_WARN_ON(waiter->task != task);
-	DEBUG_LOCKS_WARN_ON(task->blocked_on != waiter);
-	task->blocked_on = NULL;
-
-	list_del_init(&waiter->list);
-	waiter->task = NULL;
-}
-
-void debug_mutex_unlock(struct mutex *lock)
-{
-	if (likely(debug_locks)) {
-		DEBUG_LOCKS_WARN_ON(lock->magic != lock);
-		DEBUG_LOCKS_WARN_ON(!lock->wait_list.prev && !lock->wait_list.next);
+	head.data = 1;
+	head.next = &second;
+	second.data = 2;
+	second.next = &third;
+	third.data = 3; // assign data to third node
+	third.next = NULL;
+	
+	struct Node* itr = &head;
+	while(itr != NULL){
+	    printf("%d", itr->data);
+	    itr = itr->next;
 	}
-}
-
-void debug_mutex_init(struct mutex *lock, const char *name,
-		      struct lock_class_key *key)
-{
-}
-
-/***
- * mutex_destroy - mark a mutex unusable
- * @lock: the mutex to be destroyed
- *
- * This function marks the mutex uninitialized, and any subsequent
- * use of the mutex is forbidden. The mutex must not be locked when
- * this function is called.
- */
-void mutex_destroy(struct mutex *lock)
-{
-	DEBUG_LOCKS_WARN_ON(mutex_is_locked(lock));
-	lock->magic = NULL;
+	return 0;
 }
