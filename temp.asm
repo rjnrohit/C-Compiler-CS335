36c5fdd92b8ea234b2cb79f2ca329d7323121d51
Aman Tayal <amant@iitk.ac.in>
2021-05-09 23:28:37 +0530

    mutual recursion

--- temp.asm
+++ temp.asm
@@ -16,11 +16,11 @@ SYS_fork equ 57 ; fork
 SYS_exit equ 60 ; terminate
 SYS_creat equ 85 ; file open/create
 ;add global variables and constants
-const0 dq 1
-const1 dq 2
-const2 db "3123", NULL
+const0 dq 0
+const1 db "%ld", NULL
+const2 db "Enter a positive number",LF, NULL
 const3 db "%ld",LF, NULL
-const4 dq 0
+const4 dq 1
 ;add bss section for unintialized variables
 section .bss
 ;add extern symbols
@@ -37,6 +37,20 @@ extern strcpy
 extern strcat
 extern strcmp
 extern strlwr
+extern memchr
+extern memcmp
+extern memcpy
+extern memmove
+extern memset
+extern strcat
+extern strncat
+extern strchr
+extern strcmp
+extern strncmp
+extern strcoll
+extern strcpy
+extern strncpy
+extern strstr
 extern strupr
 extern free
 extern acos
@@ -56,62 +70,6 @@ extern ceil
 extern fabs
 extern floor
 section .text
-global fib
-fib:
-push   rbp
-mov    rbp,rsp
-; saving the arguments values in the stack
-sub rsp, 8; adjust rsp for return entry
-sub rsp, 8
-mov qword [rsp],rdi
-;add space for symbols
-sub rsp, 41
-mov r10, qword [rbp-16]
-mov r11, qword [const0]
-cmp r10, r11
-jle label#1
-mov r10,0
-jmp label#2
-label#1:
-mov r10,1
-label#2:
-mov byte[rbp-17], r10b
-mov r10b, byte [rbp-17]
-cmp r10, 0
-je label#0
-;copy return value in rax
-mov rax , qword[rbp-16]
-leave
-ret
-label#0:
-mov r10, qword [rbp-16]
-mov r11, qword [const0]
-sub r10, r11
-mov qword[rbp-25], r10
-; saving arguments for call
-mov rdi, qword [rbp-25]
-call fib
-;copy return value from rax
-mov qword[rbp-33], rax
-add rsp,0
-mov r10, qword [rbp-16]
-mov r11, qword [const1]
-sub r10, r11
-mov qword[rbp-41], r10
-; saving arguments for call
-mov rdi, qword [rbp-41]
-call fib
-;copy return value from rax
-mov qword[rbp-49], rax
-add rsp,0
-mov r10, qword [rbp-33]
-mov r11, qword [rbp-49]
-add r10, r11
-mov qword[rbp-57], r10
-;copy return value in rax
-mov rax , qword[rbp-57]
-leave
-ret
 global main
 main:
 push   rbp
@@ -119,45 +77,167 @@ mov    rbp,rsp
 ; saving the arguments values in the stack
 sub rsp, 8; adjust rsp for return entry
 ;add space for symbols
-sub rsp, 30
-mov r10d, dword[const2+0]
-mov dword[rbp-13+0], r10d
-mov r10b, byte[const2+4]
-mov byte[rbp-13+4], r10b
-;preparing extern function atol
+sub rsp, 60
+mov r10, qword [const0]
+mov qword[rbp-16], r10
+mov r10d, dword[const1+0]
+mov dword[rbp-28+0], r10d
+lea r10, [rbp-16]
+mov qword [rbp-24], r10
+;preparing extern function scanf
 ; saving arguments for call
 and spl, 0x0
 push rax
 sub rsp, 8
-lea rdi, [rbp-13]
+mov rsi, qword [rbp-24]
+lea rdi, [rbp-28]
 xor rax, rax
-call atol
-;copy return value from rax
-mov qword[rbp-21], rax
+call scanf
 add rsp,8
 pop rax
-mov r10, qword [rbp-21]
-mov qword[rbp-29], r10
-mov r10d, dword[const3+0]
-mov dword[rbp-34+0], r10d
-mov r10b, byte[const3+4]
-mov byte[rbp-34+4], r10b
-mov r10, qword [rbp-29]
-cvtsi2ss xmm0,r10d
-movss dword[rbp-38], xmm0
+mov r10, qword [rbp-16]
+mov r11, qword [const0]
+cmp r10, r11
+jl label#3
+mov r10,0
+jmp label#4
+label#3:
+mov r10,1
+label#4:
+mov byte[rbp-29], r10b
+mov r10b, byte [rbp-29]
+cmp r10, 0
+je label#0
+mov r10, qword[const2+0]
+mov qword[rbp-54+0], r10
+mov r10, qword[const2+8]
+mov qword[rbp-54+8], r10
+mov r10, qword[const2+16]
+mov qword[rbp-54+16], r10
+mov r10b, byte[const2+24]
+mov byte[rbp-54+24], r10b
 ;preparing extern function printf
 ; saving arguments for call
 and spl, 0x0
 push rax
 sub rsp, 8
-movss xmm0, dword [rbp-38]
-cvtss2sd xmm0,xmm0
-lea rdi, [rbp-34]
-mov rax, 1
+mov rsi, qword [const0]
+lea rdi, [rbp-54]
+xor rax, rax
 call printf
 add rsp,8
 pop rax
 ;copy return value in rax
-mov rax , qword[const4]
+mov rax , qword[const0]
+leave
+ret
+label#0:
+mov r10d, dword[const3+0]
+mov dword[rbp-60+0], r10d
+mov r10b, byte[const3+4]
+mov byte[rbp-60+4], r10b
+; saving arguments for call
+mov rdi, qword [rbp-16]
+call is_odd
+;copy return value from rax
+mov byte[rbp-55], al
+add rsp,0
+mov r10b, byte [rbp-55]
+mov qword[rbp-68], r10
+;preparing extern function printf
+; saving arguments for call
+and spl, 0x0
+push rax
+sub rsp, 8
+mov rsi, qword [rbp-68]
+lea rdi, [rbp-60]
+xor rax, rax
+call printf
+add rsp,8
+pop rax
+leave
+ret
+global is_odd
+is_odd:
+push   rbp
+mov    rbp,rsp
+; saving the arguments values in the stack
+sub rsp, 1; adjust rsp for return entry
+sub rsp, 8
+mov qword [rsp],rdi
+;add space for symbols
+sub rsp, 10
+mov r10, qword [rbp-9]
+mov r11, qword [const0]
+cmp r10, r11
+je label#5
+mov r10,0
+jmp label#6
+label#5:
+mov r10,1
+label#6:
+mov byte[rbp-10], r10b
+mov r10b, byte [rbp-10]
+cmp r10, 0
+je label#1
+;copy return value in rax
+mov al , byte[const0]
+leave
+ret
+label#1:
+mov r10, qword [rbp-9]
+mov r11, qword [const4]
+sub r10, r11
+mov qword[rbp-18], r10
+; saving arguments for call
+mov rdi, qword [rbp-18]
+call is_even
+;copy return value from rax
+mov byte[rbp-19], al
+add rsp,0
+;copy return value in rax
+mov al , byte[rbp-19]
+leave
+ret
+global is_even
+is_even:
+push   rbp
+mov    rbp,rsp
+; saving the arguments values in the stack
+sub rsp, 1; adjust rsp for return entry
+sub rsp, 8
+mov qword [rsp],rdi
+;add space for symbols
+sub rsp, 10
+mov r10, qword [rbp-9]
+mov r11, qword [const0]
+cmp r10, r11
+je label#7
+mov r10,0
+jmp label#8
+label#7:
+mov r10,1
+label#8:
+mov byte[rbp-10], r10b
+mov r10b, byte [rbp-10]
+cmp r10, 0
+je label#2
+;copy return value in rax
+mov al , byte[const4]
+leave
+ret
+label#2:
+mov r10, qword [rbp-9]
+mov r11, qword [const4]
+sub r10, r11
+mov qword[rbp-18], r10
+; saving arguments for call
+mov rdi, qword [rbp-18]
+call is_odd
+;copy return value from rax
+mov byte[rbp-19], al
+add rsp,0
+;copy return value in rax
+mov al , byte[rbp-19]
 leave
 ret
--- temp.c
+++ temp.c
@@ -1,24 +1,51 @@
-int fib(int n){
-    if(n<=1)return n;
-    return fib(n-1) + fib(n-2);
+// int fib(int n){
+//     if(n<=1)return n;
+//     return fib(n-1) + fib(n-2);
+// }
+
+// void printf(char s[20], float c);
+// void scanf(char s[20],int *c);
+// //ptr = (int*)malloc(n * sizeof(int));
+// void free(int *ptr);
+// void exit(int a);
+// int *malloc(int size);
+// int puts(char a[10]);
+// float sqrt(float c);
+// float cos(float d);
+// float ceil(float d);
+// int strlen(char a[20]);
+// int atol(char a[20]);
+// int main(){
+   
+//     int b = atol("3123");
+//     printf("%ld\n", b);
+    
+//     return 0;
+// }
+
+bool is_odd(int n);
+bool is_even(int n);
+
+void printf(char s[100],int a);
+void scanf(char s[100],int *a);
+
+int main(){
+    int n = 0;
+    scanf("%ld",&n);
+    if(n < 0){
+        printf("Enter a positive number\n",0);
+        return 0;
+    }
+    printf("%ld\n",is_odd(n));
 }
 
-void printf(char s[20], float c);
-void scanf(char s[20],int *c);
-//ptr = (int*)malloc(n * sizeof(int));
-void free(int *ptr);
-void exit(int a);
-int *malloc(int size);
-int puts(char a[10]);
-float sqrt(float c);
-float cos(float d);
-float ceil(float d);
-int strlen(char a[20]);
-int atol(char a[20]);
-int main(){
-   
-    int b = atol("3123");
-    printf("%ld\n", b);
-    
-    return 0;
+bool is_odd(int n){
+    if( n == 0) return false;
+    return is_even(n-1);
 }
+
+bool is_even(int n){
+    if( n == 0) return true;
+    return is_odd(n-1);
+}
+
