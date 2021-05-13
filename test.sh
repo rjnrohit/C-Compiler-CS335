43ce249105724e1615a99a440b12d0dd8f6bac42
Rohit Ranjan <rohitrjn629@gmail.com>
2021-05-13 17:52:45 +0530

    delete output

--- test.sh
+++ test.sh
@@ -15,9 +15,9 @@ do
         arcx86 test/$fname.c -o output/$fname -f output/$fname -c -d -a -s -t -l
         if [ -e input/$fname.in ]
         then
-            ./a.out < input/$fname.in > output/$fname.txt
+            ./output/$fname < input/$fname.in > output/$fname.txt
         else
-            ./a.out > output/$fname.txt
+            ./output/$fname > output/$fname.txt
         fi
     else 
         continue
